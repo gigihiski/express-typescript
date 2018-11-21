1370c23418b811012c0a7183dbd442ead0ab97a5
Gigih Iski Prasetyawan <gigihiski@gmail.com>
2018-11-21 22:17:49 +0800

    +add bodyparser to accept data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { Main } from "./routes/index";
 import { Welcome } from "./routes/welcome";
 
 const app: express.Application = express();
 const port: number = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/", Main);
 app.use("/welcome", Welcome);
