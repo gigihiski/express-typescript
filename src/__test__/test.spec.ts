1fab8c7b473ee2fd5918c0a57a3d550267e5eba8
Gigih Iski Prasetyawan <i@gigihiski.com>
2021-05-05 23:43:29 +0700

    🔧 jest unit test configuration

---
+++ src/__test__/test.spec.ts
@@ -0,0 +1,4 @@
+describe("Unit Test", () => {
+  it("Should return error when requested data is not valid", async () => {});
+  it("Should return success when requested data is valid", async () => {});
+});
